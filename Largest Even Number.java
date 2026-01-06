777728bbbd96cc963c55a6d0f3aaa062eb93c5bb
anuragrajput23 <anuragtwo3@gmail.com>
2026-01-06 16:12:28 +0530

    optimal solution

--- Largest Even Number.java
+++ Largest Even Number.java
@@ -1,17 +1,33 @@
+// class Solution {
+//     public String largestEven(String s) {
+//         int n = s.length();
+//         char[] arr = s.toCharArray();
+//         int i=0;
+//         long num = 0;
+//         long maxi = Integer.MIN_VALUE;
+//         while(i<n){
+//             num = num*10 + arr[i]-'0';
+//             if(num%2==0) maxi = Math.max(maxi, num);
+//             i++;
+//         }
+//         if(maxi==Integer.MIN_VALUE) return "";
+//         return maxi+"";
+
+//     }
+// }
+
 class Solution {
     public String largestEven(String s) {
-        int n = s.length();
-        char[] arr = s.toCharArray();
-        int i=0;
-        long num = 0;
-        long maxi = Integer.MIN_VALUE;
-        while(i<n){
-            num = num*10 + arr[i]-'0';
-            if(num%2==0) maxi = Math.max(maxi, num);
-            i++;
+        int n= s.length();
+        int id = -1;
+        for(int i=n-1; i>=0; i--){
+            int check = s.charAt(i)-'0';
+            if(check%2==0){
+                id = i;
+                break;
+            }
         }
-        if(maxi==Integer.MIN_VALUE) return "";
-        return maxi+"";
-
+        if(id==-1) return "";
+        return s.substring(0,id+1);
     }
 }
