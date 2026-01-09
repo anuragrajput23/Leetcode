7e094e6fb366149a42bec5c1971d5d53f0491bb7
anuragrajput23 <anuragtwo3@gmail.com>
2026-01-10 00:11:01 +0530

    binary tree deepest node finding

---
+++ Smallest Subtree with all the Deepest Nodes.java
@@ -0,0 +1,24 @@
+class Solution {
+    HashMap<Integer, Integer> map = new HashMap<>();
+    int maxD = 0;
+    public TreeNode subtreeWithAllDeepest(TreeNode root) {
+        depth(root,0);
+        return lca(root);
+    }
+    public void depth(TreeNode root, int d){
+        if(root==null) return;
+        map.put(root.val, d);
+        maxD = Math.max(d, maxD);
+        depth(root.left,d+1);
+        depth(root.right,d+1);
+    }
+    public TreeNode lca(TreeNode root){
+        if(root==null || map.getOrDefault(root.val,0)==maxD) return root;
+
+        TreeNode leftN = lca(root.left);
+        TreeNode rightN = lca(root.right);
+        if(leftN!=null && rightN!=null) return root;
+        if(leftN!=null) return leftN;
+        return rightN;
+    }
+}
