7f60a793475a88479eabd7906d55fff06f3ac442
anuragrajput23 <anuragtwo3@gmail.com>
2026-01-22 23:52:37 +0530

    Tabulation approach for Unique Paths

--- Unique Path.java
+++ Unique Path.java
@@ -1,18 +1,38 @@
+// class Solution {
+//     int dp[][];
+//     public int uniquePaths(int m, int n) {
+//         dp = new int[m][n];
+//         for(int[] row : dp) Arrays.fill(row,-1);
+//         return solve(0,0,m,n);
+//     }
+//     public int solve(int i, int j, int m, int n){
+//         if(i==m || j==n) return 0;
+//         if(i==m-1 && j==n-1){
+//             return 1;
+//         }
+//         if(dp[i][j]!=-1) return dp[i][j];
+//         int rightward = solve(i+1,j,m,n);
+//         int downward = solve(i,j+1,m,n);
+//         return dp[i][j]= rightward + downward;
+//     }
+// }
+
+//Tabulation way
 class Solution {
-    int dp[][];
     public int uniquePaths(int m, int n) {
-        dp = new int[m][n];
-        for(int[] row : dp) Arrays.fill(row,-1);
-        return solve(0,0,m,n);
-    }
-    public int solve(int i, int j, int m, int n){
-        if(i==m || j==n) return 0;
-        if(i==m-1 && j==n-1){
-            return 1;
+    int dp[][] = dp = new int[m][n];
+        for(int i=0; i<m; i++){
+            for(int j=0; j<n; j++){
+                if(i==0 || j==0) dp[i][j] = 1;
+            }
         }
-        if(dp[i][j]!=-1) return dp[i][j];
-        int rightward = solve(i+1,j,m,n);
-        int downward = solve(i,j+1,m,n);
-        return dp[i][j]= rightward + downward;
+        for(int i=1; i<m; i++){
+            for(int j=1; j<n; j++){
+                dp[i][j] = dp[i-1][j] + dp[i][j-1];
+            }
+        }
+        return dp[m-1][n-1];
+
     }
+
 }
