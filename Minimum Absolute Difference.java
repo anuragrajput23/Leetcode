46f21de40b3e95cfc340bf9c46067debffc59528
anuragrajput23 <anuragtwo3@gmail.com>
2026-01-26 23:41:23 +0530

    space optimised

--- Minimum Absolute Difference.java
+++ Minimum Absolute Difference.java
@@ -1,3 +1,24 @@
+// class Solution {
+//     public List<List<Integer>> minimumAbsDifference(int[] arr) {
+//         Arrays.sort(arr);
+//         int n = arr.length;
+//         List<List<Integer>> ans= new ArrayList<>();
+//         int check_mini = Integer.MAX_VALUE;
+//         for(int i=1;i<n; i++){
+//             check_mini = Math.min(Math.abs(arr[i]-arr[i-1]),check_mini);
+//         }
+//         for(int i=1; i<n; i++){
+//         List<Integer> list = new ArrayList<>();
+//             if(Math.abs(arr[i-1]-arr[i])==check_mini){
+//                 list.add(arr[i-1]);
+//                 list.add(arr[i]);
+//                 ans.add(list);
+//             }
+//         }
+//         return ans;
+//     }
+// }
+
 class Solution {
     public List<List<Integer>> minimumAbsDifference(int[] arr) {
         Arrays.sort(arr);
@@ -8,11 +29,11 @@ class Solution {
             check_mini = Math.min(Math.abs(arr[i]-arr[i-1]),check_mini);
         }
         for(int i=1; i<n; i++){
-        List<Integer> list = new ArrayList<>();
+        // List<Integer> list = new ArrayList<>();
             if(Math.abs(arr[i-1]-arr[i])==check_mini){
-                list.add(arr[i-1]);
-                list.add(arr[i]);
-                ans.add(list);
+                // list.add(arr[i-1]);
+                // list.add(arr[i]);
+                ans.add(List.of(arr[i-1],arr[i]));
             }
         }
         return ans;
