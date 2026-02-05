95801a5bb7995bc91aefb10548d84fcabe75d94c
anuragrajput23 <anuragtwo3@gmail.com>
2026-02-05 20:33:46 +0530

    Array Modulus

---
+++ Transformed Array.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int[] constructTransformedArray(int[] nums) {
+        int n = nums.length;
+        int[] result = new int[n];
+        for(int i=0; i<n; i++){
+            if(nums[i] > 0){
+                result[i] = nums[(i+nums[i]%n)%n];
+            }
+            else if(nums[i] < 0){
+                int position = i- Math.abs(nums[i]);
+                while(position < 0) position += n;
+                result[i] = nums[position%n];
+            }
+            else{
+                result[i] = nums[i];
+            }
+        }
+        return result;
+    }
+}
