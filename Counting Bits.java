e9490a00184221853f74144f7ca62c1674d326aa
anuragrajput23 <anuragtwo3@gmail.com>
2026-02-13 20:43:26 +0530

    do manullay

--- Counting Bits.java
+++ Counting Bits.java
@@ -1,9 +1,28 @@
+// class Solution {
+//     public int[] countBits(int n) {
+//         int[] ans = new int[n+1];
+//         for(int i=0; i<n+1; i++){
+//             ans[i] = Integer.bitCount(i);
+//         }
+//     return ans;
+//     }
+// }
+
+//without using built-in..
 class Solution {
     public int[] countBits(int n) {
         int[] ans = new int[n+1];
         for(int i=0; i<n+1; i++){
-            ans[i] = Integer.bitCount(i);
+            ans[i] = solve(i);
         }
     return ans;
+    }
+    public int solve(int n){
+        int count = 0;
+        while(n!=0){
+            count += (n & 1);
+            n = n>>1;
+        }
+        return count;
     }
 }
