57d424d0f11f1ec91037b28ff8dec53cc42e468f
anuragrajput23 <anuragtwo3@gmail.com>
2026-02-14 23:31:32 +0530

    another way

--- Number of 1 Bits.java
+++ Number of 1 Bits.java
@@ -33,13 +33,19 @@
 //     }
 // }
 
+// class Solution {
+//     public int hammingWeight(int n) {
+//         int cnt = 0;
+//         while(n > 0){
+//             cnt += n%2;
+//             n = n/2;
+//         }
+//         return cnt;
+//     }
+// }
+
 class Solution {
     public int hammingWeight(int n) {
-        int cnt = 0;
-        while(n > 0){
-            cnt += n%2;
-            n = n/2;
-        }
-        return cnt;
+        return Integer.bitCount(n);
     }
 }
