d15639e4591d1fdcb77444bb9472603f92dbdec8
anuragrajput23 <anuragtwo3@gmail.com>
2026-02-20 17:41:58 +0530

    Space Optimised

--- Binary Number with Alternating Bits.java
+++ Binary Number with Alternating Bits.java
@@ -1,14 +1,30 @@
+// class Solution {
+//     public boolean hasAlternatingBits(int n) {
+//         StringBuilder sb = new StringBuilder();
+//         while(n > 0){
+//             sb.append(n%2);
+//             n = n/2;
+//         }
+//         for(int i=1; i<sb.length(); i++){
+//             if(sb.charAt(i-1)==sb.charAt(i)){
+//                 return false;
+//             }
+//         }
+//         return true;
+//     }
+// }
+
+//without using space
 class Solution {
     public boolean hasAlternatingBits(int n) {
-        StringBuilder sb = new StringBuilder();
+        if(n==0) return true;
+        int curr = n%2;
+        n = n/2;
         while(n > 0){
-            sb.append(n%2);
+            int temp = n%2;
+            if(curr==temp) return false;
+            curr = temp;
             n = n/2;
-        }
-        for(int i=1; i<sb.length(); i++){
-            if(sb.charAt(i-1)==sb.charAt(i)){
-                return false;
-            }
         }
         return true;
     }
