d1d3359a658f394aae51184d719290bf72190d05
anuragrajput23 <anuragtwo3@gmail.com>
2026-02-23 14:57:38 +0530

    No need to generate all substring

--- Check If a String Contains All Binary Codes of Size K.java
+++ Check If a String Contains All Binary Codes of Size K.java
@@ -1,3 +1,25 @@
+// class Solution {
+//     public boolean hasAllCodes(String s, int k) {
+//         int n = s.length();
+//         List<String> list = new ArrayList<>();
+//         HashSet<String> set = new HashSet<>();
+//         for(int i=0; i<=n-k; i++){
+//             set.add(s.substring(i,i+k));
+//         }
+//         solve("",k,list);
+//         return list.size()==set.size();
+//     }
+//     public void solve(String str, int k, List<String> list){
+//         if(str.length()==k){
+//             list.add(str);
+//             return;
+//         }
+//         solve(str+"0",k,list);
+//         solve(str+"1",k,list);
+//     }
+// }
+
+//No need to generate all substring
 class Solution {
     public boolean hasAllCodes(String s, int k) {
         int n = s.length();
@@ -6,15 +28,7 @@ class Solution {
         for(int i=0; i<=n-k; i++){
             set.add(s.substring(i,i+k));
         }
-        solve("",k,list);
-        return list.size()==set.size();
+        return 1 << k ==set.size();
     }
-    public void solve(String str, int k, List<String> list){
-        if(str.length()==k){
-            list.add(str);
-            return;
-        }
-        solve(str+"0",k,list);
-        solve(str+"1",k,list);
-    }
+
 }
