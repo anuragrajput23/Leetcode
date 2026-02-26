53d4d8e59f29789e4e0da82b5cc18ad51952a703
anuragrajput23 <anuragtwo3@gmail.com>
2026-02-26 20:00:37 +0530

    Bit Manipulation

---
+++ Number of Steps to Reduce a Number in Binary Representation to One.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int numSteps(String s) {
+        StringBuilder sb = new StringBuilder(s);
+        int n  = sb.length();
+        int oper = 0;
+        while(sb.length()>1){
+            if(sb.charAt(sb.length()-1)=='0'){
+                sb.deleteCharAt(sb.length()-1);
+                oper++;
+            }else{
+                int i=sb.length()-1;
+                while(i>=0 && sb.charAt(i)=='1'){
+                    sb.setCharAt(i,'0');
+                    i--;
+                }
+                if(i>=0){
+                    sb.setCharAt(i,'1');
+                }else{
+                    sb.insert(0,'1');
+                }
+                oper++;
+            }
+        }
+        return oper;
+    }
+}
