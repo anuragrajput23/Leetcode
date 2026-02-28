ea06c713e02441477902039c3ca8d1e2348421bb
anuragrajput23 <anuragtwo3@gmail.com>
2026-02-28 23:52:12 +0530

    another way

--- Concatenation of Consecutive Binary Numbers.java
+++ Concatenation of Consecutive Binary Numbers.java
@@ -9,3 +9,17 @@ class Solution {
         return (int)result;
     }
 }
+
+//Approach 2
+class Solution {
+    public int concatenatedBinary(int n) {
+        int mod = 1000000007;
+        long result = 0;
+        int digit = 0;
+        for(int i=1; i<=n; i++){
+            if((i & (i-1))==0) {digit++;}
+            result = ((result<<digit)%mod +i)%mod;
+        }
+        return (int)result;
+    }
+}
