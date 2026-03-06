cfc34fdedbb7319dde5e208d0408bea12125ebe5
anuragrajput23 <anuragtwo3@gmail.com>
2026-03-06 22:51:02 +0530

    another approach

--- Check if Binary String Has at Most One Segment of Ones.java
+++ Check if Binary String Has at Most One Segment of Ones.java
@@ -1,18 +1,26 @@
+// class Solution {
+//     public boolean checkOnesSegment(String s) {
+//         int n = s.length();
+//         int cnt = 0;
+//         int i=0;
+//         while(i<n){
+//             if(s.charAt(i)=='1'){
+//                 cnt++;
+//                 if(cnt>1) return false;
+//                 while(i<n && s.charAt(i)=='1'){
+//                     i++;
+//                 }
+//             }
+//             i++;
+//         }
+//         return true;
+//     }
+// }
+
+//Method -02
 class Solution {
     public boolean checkOnesSegment(String s) {
-        int n = s.length();
-        int cnt = 0;
-        int i=0;
-        while(i<n){
-            if(s.charAt(i)=='1'){
-                cnt++;
-                if(cnt>1) return false;
-                while(i<n && s.charAt(i)=='1'){
-                    i++;
-                }
-            }
-            i++;
-        }
+        if(s.contains("01")) return false;
         return true;
     }
 }
