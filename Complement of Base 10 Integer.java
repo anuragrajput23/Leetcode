4b9ea018a701ed6b3603c28b4db60e2486b28256
anuragrajput23 <anuragtwo3@gmail.com>
2026-03-11 20:51:46 +0530

    another way

--- Complement of Base 10 Integer.java
+++ Complement of Base 10 Integer.java
@@ -13,3 +13,14 @@ class Solution {
         return result;
     }
 }
+
+//M2
+class Solution {
+    public int bitwiseComplement(int n) {
+        int mask = 1;
+        while(mask < n){
+            mask = (mask << 1) | 1;
+        }
+        return mask^n;
+    }
+}
