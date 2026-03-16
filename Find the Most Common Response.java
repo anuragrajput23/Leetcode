6a0eba461263d7cfabff78d63c5b2b323a109de3
anuragrajput23 <anuragtwo3@gmail.com>
2026-03-16 20:51:30 +0530

    optimised one

--- Find the Most Common Response.java
+++ Find the Most Common Response.java
@@ -27,3 +27,32 @@ class Solution {
         return lexi.get(0);
     }
 }
+//optimised one
+class Solution {
+    public String findCommonResponse(List<List<String>> res) {
+        HashMap<String,Integer> map = new HashMap<>();
+        int m = res.size();
+        for(int i=0; i<m; i++){
+            HashSet<String> set = new HashSet<>();
+            List<String> l = res.get(i);
+            for(int j=0; j<l.size(); j++){
+                String str = l.get(j);
+                if(!set.contains(str)){
+                    map.put(str,map.getOrDefault(str,0)+1);
+                }
+                set.add(str);
+            }
+        }
+        int maxStr = 0;
+        String str = "~";
+        for(String key : map.keySet()){
+            maxStr = Math.max(maxStr, map.get(key));
+        }
+        for(String s : map.keySet()){
+            if(map.get(s)==maxStr && s.compareTo(str) < 0){
+                str = s;
+            }
+        }
+        return str;
+    }
+}
