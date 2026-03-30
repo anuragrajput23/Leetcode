df8105bcd74d4bf2700b0a1cf11b237434003bbe
anuragrajput23 <anuragtwo3@gmail.com>
2026-03-30 11:49:54 +0530

    freq array used

--- Check if Strings Can be Made Equal With Operations I.java
+++ Check if Strings Can be Made Equal With Operations I.java
@@ -26,10 +26,33 @@
 // }
 
 //M2
+// class Solution {
+//     public boolean canBeEqual(String s1, String s2) {
+//         return ((s1.charAt(0)==s2.charAt(0) || s1.charAt(0)==s2.charAt(2)) &&
+//         (s1.charAt(1)==s2.charAt(3) || s1.charAt(1)==s2.charAt(1)) && (s1.charAt(3)==s2.charAt(1) || s1.charAt(3)==s2.charAt(3)) && (s1.charAt(2)==s2.charAt(2) || s1.charAt(2)==s2.charAt(0))
+//         );
+//     }
+// }
+
+//Method 3
 class Solution {
     public boolean canBeEqual(String s1, String s2) {
-        return ((s1.charAt(0)==s2.charAt(0) || s1.charAt(0)==s2.charAt(2)) &&
-        (s1.charAt(1)==s2.charAt(3) || s1.charAt(1)==s2.charAt(1)) && (s1.charAt(3)==s2.charAt(1) || s1.charAt(3)==s2.charAt(3)) && (s1.charAt(2)==s2.charAt(2) || s1.charAt(2)==s2.charAt(0))
-        );
+        int[] even = new int[26];
+        int[] odd = new int[26];
+        for(int i=0; i<4; i++){
+            if(i%2==0){
+                even[s1.charAt(i)-'a']++;
+                even[s2.charAt(i)-'a']--;
+            }else{
+                odd[s1.charAt(i)-'a']++;
+                odd[s2.charAt(i)-'a']--;
+            }
+        }
+        for(int i=0; i<26; i++){
+            if(odd[i]!=0 || even[i]!=0){
+                return false;
+            }
+        }
+        return true;
     }
 }
