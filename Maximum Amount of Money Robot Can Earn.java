9e063d528b6762c220e95eec527470438e655f21
anuragrajput23 <anuragtwo3@gmail.com>
2026-04-03 13:48:04 +0530

    using null in place to -1 to avoid conflict

--- Maximum Amount of Money Robot Can Earn.java
+++ Maximum Amount of Money Robot Can Earn.java
@@ -41,19 +41,60 @@
 // }
 
 //Memisation:
+// class Solution {
+//     int[][][] dp;
+//     public int maximumAmount(int[][] coins) {
+//         int m = coins.length;
+//         int n = coins[0].length;
+//         dp = new int[m][n][3];
+//         for(int i=0; i<m; i++){
+//             for(int j=0; j<n; j++){
+//                 for(int k=0; k<3; k++){
+//                     dp[i][j][k] = -1;
+//                 }
+//             }
+//         }
+//         return solve(0,0,m,n,2,coins);
+//     }
+//     public int solve(int i, int j, int m, int n, int neutro, int[][] coins){
+//         if(i>=m || j>=n) return Integer.MIN_VALUE;
+//         if(i==m-1 && j==n-1){
+//             if(coins[i][j] >= 0){
+//                 return coins[i][j];
+//             }
+//             return (neutro > 0) ? 0 : coins[i][j];
+//         }
+//         if(dp[i][j][neutro]!=-1) return dp[i][j][neutro];
+//         int take = Integer.MIN_VALUE;
+//         if(coins[i][j] >= 0){
+//             take =coins[i][j]+ Math.max(solve(i,j+1,m,n,neutro,coins),solve(i+1,j,m,n,neutro,coins));
+//         }
+//         int ntake = Integer.MIN_VALUE;
+//         if(coins[i][j] < 0){
+//             ntake = coins[i][j] + Math.max(solve(i+1,j,m,n,neutro,coins),solve(i,j+1,m,n,neutro,coins));
+//         }
+//         int absolute = Integer.MIN_VALUE;
+//         if(coins[i][j] < 0 && neutro > 0){
+//             absolute = Math.max(solve(i+1,j,m,n,neutro-1,coins),solve(i,j+1,m,n,neutro-1,coins));
+//         }
+//         return dp[i][j][neutro] =Math.max(absolute,Math.max(ntake,take));
+//     }
+// }
+
+//using null in place to -1 to avoid conflict :
 class Solution {
-    int[][][] dp;
+    Integer[][][] dp;
     public int maximumAmount(int[][] coins) {
         int m = coins.length;
         int n = coins[0].length;
-        dp = new int[m][n][3];
-        for(int i=0; i<m; i++){
-            for(int j=0; j<n; j++){
-                for(int k=0; k<3; k++){
-                    dp[i][j][k] = -1;
-                }
-            }
-        }
+        dp = new Integer[m][n][3];
+        // for(int i=0; i<m; i++){
+        //     for(int j=0; j<n; j++){
+        //         for(int k=0; k<3; k++){
+        //             dp[i][j][k] = -1;
+        //         }
+        //     }
+        // }
         return solve(0,0,m,n,2,coins);
     }
     public int solve(int i, int j, int m, int n, int neutro, int[][] coins){
@@ -64,7 +105,7 @@ class Solution {
             }
             return (neutro > 0) ? 0 : coins[i][j];
         }
-        if(dp[i][j][neutro]!=-1) return dp[i][j][neutro];
+        if(dp[i][j][neutro]!=null) return dp[i][j][neutro];
         int take = Integer.MIN_VALUE;
         if(coins[i][j] >= 0){
             take =coins[i][j]+ Math.max(solve(i,j+1,m,n,neutro,coins),solve(i+1,j,m,n,neutro,coins));
