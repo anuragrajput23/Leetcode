7eb9a3236ff6f9813eabe8e02f9fb8034a6afd41
anuragrajput23 <anuragtwo3@gmail.com>
2026-04-09 20:42:30 +0530

    Heap

--- Bulls and Cows.java
+++ Bulls and Cows.java
@@ -35,7 +35,6 @@ class Solution {
                 }
             }
         }
-
         return bulls+"A"+cow+"B";
     }
 }
---
+++ Top K Frequent Elements.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int[] topKFrequent(int[] nums, int k) {
+        int n = nums.length;
+        HashMap<Integer,Integer> map = new HashMap<>();
+        for(int ele : nums) map.put(ele, map.getOrDefault(ele,0)+1);
+        PriorityQueue<int[]> pq = new PriorityQueue<>(((a,b)-> b[1]-a[1]));
+        for(int key : map.keySet()){
+            pq.offer(new int[]{key,map.get(key)});
+        }
+        int[] ans = new int[k];
+        for(int i=0; i<k; i++){
+            ans[i] = pq.poll()[0];
+        }
+        return ans;
+    }
+}
