d2fafb0017a8dcd6510b9389213d419562082fa8
anuragrajput23 <anuragtwo3@gmail.com>
2026-04-12 22:59:52 +0530

    better iteration

--- Perfect Number.java
+++ Perfect Number.java
@@ -9,3 +9,15 @@ class Solution {
         return sum==num;
     }
 }
+
+class Solution {
+    public boolean checkPerfectNumber(int num) {
+        int sum = 0;
+        for(int i=1; i<=Math.sqrt(num); i++){
+            if(num%i==0){
+                sum += i;
+            }
+        }
+        return sum==num;
+    }
+}
