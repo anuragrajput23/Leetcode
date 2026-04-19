f9910610f30a6928633001fe1ff5ffba97b5d502
anuragrajput23 <anuragtwo3@gmail.com>
2026-04-19 23:00:40 +0530

    Bottom-up

--- Climbing Stairs.java
+++ Climbing Stairs.java
@@ -12,18 +12,34 @@
 // }
 
 //Memoisation
+// class Solution {
+//     public int climbStairs(int n) {
+//         int[] dp = new int[n+1];
+//         Arrays.fill(dp,-1);
+//         return solve(n,dp);
+//     }
+//     public int solve(int n, int[] dp){
+//         if(n==0) return 1;
+//         if(n<0) return 0;
+//         if(dp[n]!=-1) return dp[n];
+//         int step1 = solve(n-1, dp);
+//         int step2 = solve(n-2, dp);
+//         return dp[n]= step1+step2;
+//     }
+// }
+
+//Bottom -up
 class Solution {
+    int[] dp;
     public int climbStairs(int n) {
-        int[] dp = new int[n+1];
-        Arrays.fill(dp,-1);
-        return solve(n,dp);
+        dp = new int[n];
+        dp[0] = 1;
+        if(n==1) return 1;
+        dp[1] = 2;
+        for(int i=2; i<n; i++){
+            dp[i] = dp[i-1] + dp[i-2];
+        }
+        return dp[n-1];
     }
-    public int solve(int n, int[] dp){
-        if(n==0) return 1;
-        if(n<0) return 0;
-        if(dp[n]!=-1) return dp[n];
-        int step1 = solve(n-1, dp);
-        int step2 = solve(n-2, dp);
-        return dp[n]= step1+step2;
-    }
+
 }
