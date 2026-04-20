e95dbfec4ccf8ea911ab626260042a56b0a0d918
anuragrajput23 <anuragtwo3@gmail.com>
2026-04-20 17:09:25 +0530

    less iteration

--- Two Furthest Houses With Different Colors.java
+++ Two Furthest Houses With Different Colors.java
@@ -10,3 +10,20 @@ class Solution {
         return maxi;
     }
 }
+
+//Slight less iteration
+class Solution {
+    public int maxDistance(int[] colors) {
+        int n = colors.length;
+        int maxi=0;
+        for(int i=0; i<n; i++){
+            for(int j=n-1; j>=i; j--){
+                if(colors[i]!=colors[j]){
+                    maxi = Math.max(maxi,Math.abs(i-j));
+                    break;
+                }
+            }
+        }
+        return maxi;
+    }
+}
