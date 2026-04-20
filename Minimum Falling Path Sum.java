4b3b4e541c26decd078d52e4a6cba55c97e75138
anuragrajput23 <anuragtwo3@gmail.com>
2026-04-20 20:45:58 +0530

    Bottom-up

--- Minimum Falling Path Sum.java
+++ Minimum Falling Path Sum.java
@@ -28,3 +28,29 @@ class Solution {
         return dp[i][j] =  matrix[i][j]+ Math.min(Math.min(diag,diag2), belo);
     }
 }
+
+//Bottom -up
+class Solution {
+    public int minFallingPathSum(int[][] matrix) {
+        int m = matrix.length;
+        int n = matrix[0].length;
+        int[][] dp = new int[m][n];
+        for(int j=0; j<n; j++){
+            dp[0][j] = matrix[0][j];
+        }
+        for(int i=1; i<m; i++){
+            for(int j=0; j<n; j++){
+                int rmcm = (j-1>=0) ? dp[i-1][j-1] : Integer.MAX_VALUE;
+                int rmc = (i-1 >= 0) ? dp[i-1][j] : Integer.MAX_VALUE;
+                int rmcp = (j+1 <=n-1) ? dp[i-1][j+1] : Integer.MAX_VALUE;
+                dp[i][j] = matrix[i][j] + Math.min(rmcm, Math.min(rmc,rmcp));
+            }
+        }
+        int ans = Integer.MAX_VALUE;
+        for(int j = 0; j < n; j++){
+            ans = Math.min(ans, dp[m-1][j]);
+        }
+        return ans;
+
+    }
+}
