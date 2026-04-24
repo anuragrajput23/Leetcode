ae95d6808b32b3d7de74ab1b6225372b772b7599
anuragrajput23 <anuragtwo3@gmail.com>
2026-04-24 11:38:07 +0530

    O(n)

--- Two Furthest Houses With Different Colors.java
+++ Two Furthest Houses With Different Colors.java
@@ -12,16 +12,45 @@ class Solution {
 }
 
 //Slight less iteration
+// class Solution {
+//     public int maxDistance(int[] colors) {
+//         int n = colors.length;
+//         int maxi=0;
+//         for(int i=0; i<n; i++){
+//             for(int j=n-1; j>=i; j--){
+//                 if(colors[i]!=colors[j]){
+//                     maxi = Math.max(maxi,Math.abs(i-j));
+//                     break;
+//                 }
+//             }
+//         }
+//         return maxi;
+//     }
+// }
+
+//more optimised in O(n)
 class Solution {
     public int maxDistance(int[] colors) {
         int n = colors.length;
-        int maxi=0;
-        for(int i=0; i<n; i++){
-            for(int j=n-1; j>=i; j--){
-                if(colors[i]!=colors[j]){
-                    maxi = Math.max(maxi,Math.abs(i-j));
-                    break;
-                }
+        int maxi = 0;
+        int j=n-1;
+        int i=0;
+        while(i<=j){
+            if(colors[i]!=colors[j]){
+                maxi = Math.max(maxi,Math.abs(j-i));
+                break;
+            }else{
+                i++;
+            }
+        }
+        i=0;
+        j=n-1;
+        while(j>=i){
+            if(colors[i]!=colors[j]){
+                maxi = Math.max(maxi,Math.abs(j-i));
+                break;
+            }else{
+                j--;
             }
         }
         return maxi;
