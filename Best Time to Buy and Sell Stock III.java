644fe8d42ff418bf1fb09055d4b017d6a9d8a739
anuragrajput23 <anuragtwo3@gmail.com>
2026-04-26 15:16:21 +0530

    Memoisation

--- Best Time to Buy and Sell Stock III.java
+++ Best Time to Buy and Sell Stock III.java
@@ -17,3 +17,25 @@ class Solution {
         return profit;
     }
 }
+
+class Solution {
+    Integer[][][] dp;
+    public int maxProfit(int[] prices) {
+        int n = prices.length;
+        dp = new Integer[n][2][3];
+        return solve(prices,0,1,2);
+    }
+    public int solve(int[] prices, int i, int flag, int limit){
+        int profit = 0;
+        if(limit==0) return 0;
+        if(i==prices.length) return 0;
+        if(dp[i][flag][limit]!=null) return dp[i][flag][limit];
+
+        if(flag==1){
+            profit += Math.max(-prices[i]+solve(prices,i+1,0,limit), solve(prices,i+1,1,limit));
+        }else{
+              profit += Math.max(prices[i]+solve(prices,i+1,1,limit-1),solve(prices,i+1,0,limit));
+        }
+        return dp[i][flag][limit] =profit;
+    }
+}
