63442dde4a84891d0d37f29937347d5538a12019
anuragrajput23 <anuragtwo3@gmail.com>
2026-04-26 21:29:48 +0530

    recursive way

---
+++ Best Time to Buy and Sell Stock with Cooldown.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+        int n = prices.length;
+        return solve(prices,0,1);
+    }
+    public int solve(int[] prices, int i, int flag){
+        int profit = 0;
+        if(i>=prices.length) return 0;
+        if(flag==1){
+            profit += Math.max(-prices[i] + solve(prices, i+1, 0), solve(prices, i+1, 1));
+        }else{
+            profit += Math.max(prices[i] + solve(prices, i+2, 1), solve(prices, i+1, 0));
+        }
+        return profit;
+    }
+}
