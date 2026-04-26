5beaf04881071244b2fdd3074f54888d9f3c7c6c
anuragrajput23 <anuragtwo3@gmail.com>
2026-04-26 14:57:16 +0530

    recursive approach

---
+++ Best Time to Buy and Sell Stock III.java
@@ -0,0 +1,19 @@
+//recursive approach
+class Solution {
+    public int maxProfit(int[] prices) {
+        int n = prices.length;
+        return solve(prices,0,1,2);
+    }
+    public int solve(int[] prices, int i, int flag, int limit){
+        int profit = 0;
+        if(limit==0) return 0;
+        if(i==prices.length) return 0;
+
+        if(flag==1){
+            profit += Math.max(-prices[i]+solve(prices,i+1,0,limit), solve(prices,i+1,1,limit));
+        }else{
+              profit += Math.max(prices[i]+solve(prices,i+1,1,limit-1),solve(prices,i+1,0,limit));
+        }
+        return profit;
+    }
+}
