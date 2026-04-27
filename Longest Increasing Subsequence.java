88f8c54d01f668260051cd6a197fdd653fd977f7
anuragrajput23 <anuragtwo3@gmail.com>
2026-04-27 23:48:31 +0530

    memoised

--- Longest Increasing Subsequence.java
+++ Longest Increasing Subsequence.java
@@ -14,3 +14,22 @@ class Solution {
 }
 
 //memoised
+class Solution {
+    Integer[][] dp;
+    public int lengthOfLIS(int[] nums) {
+        int n = nums.length;
+        dp = new Integer[n][n+1];
+        return solve(0,-1,nums);
+    }
+    public int solve(int i, int prev, int[] nums){
+        if(i==nums.length) return 0;
+        if(dp[i][prev+1]!=null) return dp[i][prev+1];
+        int take = 0;
+        if(prev==-1 || nums[prev] < nums[i]){
+            take = 1+solve(i+1, i, nums);
+        }
+        int ntake = solve(i+1,prev,nums);
+        return dp[i][prev+1]= Math.max(ntake,take);
+
+    }
+}
