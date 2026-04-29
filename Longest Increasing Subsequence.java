f8126d2dbe425407194d1a12bf56babba8be84fb
anuragrajput23 <anuragtwo3@gmail.com>
2026-04-29 18:13:11 +0530

    iterative way

--- Longest Increasing Subsequence.java
+++ Longest Increasing Subsequence.java
@@ -53,3 +53,22 @@ class Solution {
     }
 
 }
+
+// /Tabulation
+class Solution {
+    public int lengthOfLIS(int[] nums) {
+        int n = nums.length;
+        int[] dp = new int[n];
+        int maxiLen = 1;
+        Arrays.fill(dp,1);
+        for(int i=0; i<n; i++){
+            for(int j=0; j<i; j++){
+                if(nums[i] >  nums[j]){
+                    dp[i] = Math.max(dp[i],dp[j]+1);
+                    maxiLen = Math.max(dp[i],maxiLen);
+                }
+            }
+        }
+        return maxiLen;
+    }
+}
