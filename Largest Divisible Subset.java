1fb4c38372ec7a1d9557024f4abb1e8c0e04c522
anuragrajput23 <anuragtwo3@gmail.com>
2026-04-30 13:04:32 +0530

    tabulation

--- Largest Divisible Subset.java
+++ Largest Divisible Subset.java
@@ -22,3 +22,40 @@ class Solution {
         solve(i+1,prev,result,curr,nums);
     }
 }
+
+//optimisation
+class Solution {
+    public List<Integer> largestDivisibleSubset(int[] nums) {
+        int n = nums.length;
+        Arrays.sort(nums);
+        ArrayList<Integer> ans = new ArrayList<>();
+        int[] dp = new int[n];
+        int[] prevIdx = new int[n];
+        Arrays.fill(prevIdx,-1);
+        int maxiLen = 1;
+        int lis = -1;
+        int lisIdx = 0;
+        Arrays.fill(dp,1);
+        for(int i=0; i<n; i++){
+            for(int j=0; j<i; j++){
+                if(nums[i] %  nums[j] == 0){
+                    if(dp[j]+1 > dp[i]){
+                        dp[i] = dp[j]+1;
+                        prevIdx[i] =j;
+                        if(lis < dp[i]){
+                            lis = dp[i];
+                            lisIdx = i;
+                        }
+                    }
+                }
+            }
+        }
+        //
+        while(lisIdx!=-1){
+            ans.add(nums[lisIdx]);
+            lisIdx = prevIdx[lisIdx];
+        }
+        Collections.reverse(ans);
+        return ans;
+    }
+}
