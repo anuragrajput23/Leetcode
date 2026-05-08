a1351ae6f16a97092c0d048d81f0f51faad4346f
anuragrajput23 <anuragtwo3@gmail.com>
2026-05-08 16:54:19 +0530

    memoisation

--- House Robber.java
+++ House Robber.java
@@ -10,4 +10,19 @@ public class Solution {
         return take;
     }
 }
-
+//memoisation
+class Solution {
+    Integer[] dp;
+    public int rob(int[] nums) {
+       int n = nums.length;
+       dp = new Integer[n];
+       return solve(0,n,nums);
+    }
+    public int solve(int i, int n, int[] nums){
+        if(i>=n) return 0;
+        if(i==n-1) return nums[i];
+        if(dp[i]!=null) return dp[i];
+        int take = Math.max(nums[i] + solve(i+2,n,nums), solve(i+1,n,nums));
+        return dp[i] = take;
+    }
+}
