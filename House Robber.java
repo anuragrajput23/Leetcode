c0198fac585b3bb75b7ef22202ab795762fe1ede
anuragrajput23 <anuragtwo3@gmail.com>
2026-05-08 18:35:48 +0530

    space optimisation

--- House Robber.java
+++ House Robber.java
@@ -26,3 +26,21 @@ class Solution {
         return dp[i] = take;
     }
 }
+//space optimisation
+class Solution {
+    public int rob(int[] nums) {
+       int n = nums.length;
+       int[] dp = new int[n];
+       dp[0] = nums[0];
+
+       for(int i=1; i<n; i++){
+            int take = nums[i];
+            if(i > 1){
+                take += dp[i-2];
+            }
+            int ntake = dp[i-1];
+            dp[i] = Math.max(take,ntake);
+       }
+       return dp[n-1];
+    }
+}
