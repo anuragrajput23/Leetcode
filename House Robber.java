9ea93abc6acc3c878f776a2d0cbbda7948ea47fb
anuragrajput23 <anuragtwo3@gmail.com>
2026-05-09 00:53:30 +0530

    most optimised

--- House Robber.java
+++ House Robber.java
@@ -44,3 +44,25 @@ class Solution {
        return dp[n-1];
     }
 }
+
+//space optimised in O(1)
+class Solution {
+    public int rob(int[] nums) {
+       int n = nums.length;
+       int[] dp = new int[n];
+    int prev1 = nums[0];
+    int prev2 = 0;
+
+       for(int i=1; i<n; i++){
+            int take = nums[i];
+            if(i > 1){
+                take += prev2;
+            }
+            int ntake = prev1;
+            int curr = Math.max(take,ntake);
+            prev2=prev1;
+            prev1=curr;
+       }
+       return prev1;
+    }
+}
