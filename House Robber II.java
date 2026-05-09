a757c4c3eda2e89b2a2b23441992cd3f1fd0aed4
anuragrajput23 <anuragtwo3@gmail.com>
2026-05-10 00:19:48 +0530

    most optimised

--- House Robber II.java
+++ House Robber II.java
@@ -16,3 +16,34 @@ class Solution {
         return dp[i]= take;
     }
 }
+
+//space optimised and complexity too
+class Solution {
+    public int rob(int[] nums) {
+        int n = nums.length;
+        if(n==1) return nums[0];
+        ArrayList<Integer> list1 = new ArrayList<>();
+        ArrayList<Integer> list2 = new ArrayList<>();
+        for(int i=0; i<n; i++){
+            if(i!=0) list1.add(nums[i]);
+            if(i!=n-1) list2.add(nums[i]);
+        }
+        return Math.max(solve(list1), solve(list2));
+    }
+    public int solve(ArrayList<Integer> list){
+        int n = list.size();
+        int prev2 = 0;
+        int prev1 = list.get(0);
+        for(int i=1; i<n; i++){
+            int take = list.get(i);
+            if(i-2 >=0){
+                take += prev2;
+            }
+            int ntake = prev1;
+            int curr = Math.max(take,ntake);
+            prev2 = prev1;
+            prev1 = curr;
+        }
+        return prev1;
+    }
+}
