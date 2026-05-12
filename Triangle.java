365b881a584932c0b0dc1800371b98e8e5bf9633
anuragrajput23 <anuragtwo3@gmail.com>
2026-05-12 17:00:09 +0530

    memoisation

--- Triangle.java
+++ Triangle.java
@@ -13,3 +13,23 @@ class Solution {
         return Math.min(down, down_right);
     }
 }
+
+//memoisation:
+class Solution {
+    Integer[][] dp;
+    public int minimumTotal(List<List<Integer>> triangle) {
+        int n = triangle.size();
+        dp = new Integer[n][n];
+        if(n==1) return triangle.get(0).get(0);
+        return solve(0,0,n,triangle);
+    }
+    public int solve(int i, int j, int n, List<List<Integer>> tria){
+        if(i==n) return (int)1e9;
+        if(i==n-1) return tria.get(i).get(j);
+        if(dp[i][j]!=null) return dp[i][j];
+        int result = 0;
+        int down = tria.get(i).get(j) + solve(i+1,j,n,tria);
+        int down_right = tria.get(i).get(j) + solve(i+1,j+1,n,tria);
+        return dp[i][j]= Math.min(down, down_right);
+    }
+}
