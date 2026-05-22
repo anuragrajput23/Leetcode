a04ccd1bdad7e88945ed422e1acd0f09cdd86379
anuragrajput23 <anuragtwo3@gmail.com>
2026-05-22 23:54:29 +0530

    same as previous

--- Search in Rotated Sorted Array.java
+++ Search in Rotated Sorted Array.java
@@ -27,3 +27,29 @@ class Solution {
         return -1;
     }
 }
+
+//today POTD
+class Solution {
+    public int search(int[] nums, int target) {
+        int n = nums.length;
+        int lo=0,hi=n-1;
+        while(lo <= hi){
+            int mid = lo +(hi-lo)/2;
+            if(nums[mid]==target) return mid;
+            if(nums[lo] <= nums[mid]){
+                if(nums[mid] >= target && nums[lo] <= target){
+                    hi = mid-1;
+                }else{
+                    lo=mid+1;
+                }
+            }else{
+                if(nums[mid] <= target && nums[hi] >= target){
+                    lo=mid+1;
+                }else{
+                    hi = mid-1;
+                }
+            }
+        }
+        return -1;
+    }
+}
