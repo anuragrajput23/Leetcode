5353b3d0eba92516147a621f3583b8b72f2cb8eb
anuragrajput23 <anuragtwo3@gmail.com>
2026-05-28 11:29:43 +0530

    recursive way

---
+++ Partition Equal Subset Sum.java
@@ -0,0 +1,19 @@
+public class Solution {
+    public boolean canPartition(int[] nums) {
+        int n = nums.length;
+        int sum = 0;
+        for(int ele : nums) sum += ele;
+        if(sum%2==1) return false;
+        return solve(0,n,nums,sum/2);
+    }
+    public boolean solve(int i, int n, int[] nums, int target){
+        if(target==0) return true;
+        if(i==n) return false;
+        boolean take =  false;
+        if(nums[i] <= target){
+            take = solve(i+1,n,nums,target-nums[i]);
+        }
+        boolean ntake = solve(i+1,n,nums,target);
+        return take | ntake;
+    }
+} 
