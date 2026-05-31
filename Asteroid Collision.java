f9859dca431119a1da0084eec3aadc877218db07
anuragrajput23 <anuragtwo3@gmail.com>
2026-05-31 23:21:59 +0530

    more optimised

--- Asteroid Collision.java
+++ Asteroid Collision.java
@@ -25,3 +25,28 @@ class Solution {
         return ans;
     }
 }
+
+//more optimised
+class Solution {
+    public int[] asteroidCollision(int[] stone) {
+        int n = stone.length;
+        Stack<Integer> st = new Stack<>();
+        for(int i=0; i<n; i++){
+            boolean check = false;
+            while(!st.isEmpty() && st.peek()>0 && stone[i]<0){
+                int toPositive = -stone[i];
+                int m = st.peek();
+                if(toPositive > m) st.pop();
+                else if(toPositive == m) {st.pop(); check=true; break;}
+                else{ check = true; break;}
+            }
+            if(check==false)  st.push(stone[i]);
+        }
+        int len = st.size();
+        int[] ans = new int[len];
+        for (int i = len - 1; i >= 0; i--){
+            ans[i] = st.pop();
+        }
+        return ans;
+    }
+}
