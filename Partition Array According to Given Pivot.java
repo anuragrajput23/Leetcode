887cbb6d7152fbc40cac5f382db87e3c45662efd
anuragrajput23 <anuragtwo3@gmail.com>
2026-06-10 10:23:13 +0530

    just in 1 space

--- Partition Array According to Given Pivot.java
+++ Partition Array According to Given Pivot.java
@@ -21,3 +21,32 @@ class Solution {
         return ans;
     }
 }
+
+//another method
+class Solution {
+    public int[] pivotArray(int[] nums, int pivot) {
+        int n = nums.length;
+        int less = 0;
+        int equal = 0;
+        int large = 0;
+        int[] result = new int[n];
+        for(int ele : nums){
+            if(ele < pivot){
+                less++;
+            }else if(ele==pivot){
+                equal++;
+            }else{
+                large++;
+            }
+        }
+        int k=0;
+        int l=less;
+        int m = less+equal;
+        for(int i=0; i<n; i++){
+            if(nums[i] < pivot) result[k++]=nums[i];
+            else if(nums[i] == pivot) result[l++] = nums[i];
+            else result[m++]=nums[i];
+        }
+        return result;
+    }
+}
