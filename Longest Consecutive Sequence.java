c3a3e8101bf9808bb428e8055383b989ee245a0c
anuragrajput23 <anuragtwo3@gmail.com>
2026-06-13 23:41:58 +0530

    improved

--- Longest Consecutive Sequence.java
+++ Longest Consecutive Sequence.java
@@ -40,3 +40,26 @@ class Solution {
         return maxiLen;
     }
 }
+
+//O(n) O(N) space
+class Solution {
+    public int longestConsecutive(int[] nums) {
+        int n = nums.length;
+        if(n==0) return 0;
+        int maxiLen = 1;
+        HashSet<Integer> set = new HashSet<>();
+        for(int ele : nums) set.add(ele);
+        for(int it : set){
+            if(!set.contains(it-1)){
+                int cnt = 1;
+                int x = it;
+                while(set.contains(x+1)){
+                    x = x+1;
+                    cnt = cnt+1;
+                }
+                maxiLen = Math.max(maxiLen, cnt);
+            }
+        }
+        return maxiLen;
+    }
+}
