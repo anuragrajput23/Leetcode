c5374b1be2a192559d9852533d52d366a4823120
anuragrajput23 <anuragtwo3@gmail.com>
2026-06-20 23:48:08 +0530

    brute

---
+++ Sum of Beauty of All Substrings.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int beautySum(String s) {
+        int n = s.length();
+        int ans = 0;
+        for(int i=0; i<n; i++){
+            String str = "";
+            for(int j=i; j<n; j++){
+                str += s.charAt(j);
+                int beauty = check(str);
+                ans += beauty;
+            }
+        }
+        return ans;
+    }
+    public int check(String str){
+        HashMap<Character, Integer> map = new HashMap<>();
+        for(char ch : str.toCharArray()) map.put(ch,map.getOrDefault(ch,0)+1);
+        int mini = Integer.MAX_VALUE;
+        int maxi = Integer.MIN_VALUE;
+        for(char ch : map.keySet()){
+            mini = Math.min(mini, map.get(ch));
+            maxi = Math.max(maxi, map.get(ch));
+        }
+        return maxi-mini;
+    }
+}
