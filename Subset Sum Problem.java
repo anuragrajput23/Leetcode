49d45a66d5aa8e7b857edbe705f7d7dc4bd40f7b
anuragrajput23 <anuragtwo3@gmail.com>
2026-06-23 17:43:59 +0530

    tabulation

--- Subset Sum Problem.java
+++ Subset Sum Problem.java
@@ -67,3 +67,29 @@ class Solution {
         return dp[i][target]= ntake || take;
     }
 }
+
+//tabulation
+class Solution {
+    static Boolean isSubsetSum(int arr[], int sum) {
+        // code here
+        int n = arr.length;
+        boolean[][] dp = new boolean[n+1][sum+1];
+        for(int i=0; i<n+1; i++){
+            for(int j=0; j<sum+1; j++){
+                if(i==0) dp[i][j] = false;
+                if(j==0) dp[i][j] = true;
+            }
+        }
+        for(int i=1; i<n+1; i++){
+            for(int j=1; j<sum+1; j++){
+                boolean take = false;
+                if(arr[i-1] <= j){
+                    take = dp[i-1][j-arr[i-1]];
+                }
+                boolean ntake = dp[i-1][j];
+                dp[i][j] = ntake || take;
+            }
+        }
+        return dp[n][sum];
+    }
+}
