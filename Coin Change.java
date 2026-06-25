448e6b49bd0f6dd1906694aec6019037f3cc32ab
anuragrajput23 <anuragtwo3@gmail.com>
2026-06-25 16:54:13 +0530

    tabulation

--- Coin Change.java
+++ Coin Change.java
@@ -19,3 +19,25 @@ class Solution {
         return dp[i][amt]= Math.min(take,ntake);
     }
 }
+
+class Solution {
+    int[][] dp;
+    public int coinChange(int[] coins, int amt) {
+        int n = coins.length;
+        dp = new int[n+1][amt+1];
+        for(int i=0; i<n+1; i++) dp[i][0] = 0;
+        for(int j=1; j<amt+1; j++) dp[0][j] = (int)1e9;
+
+        for(int i=1; i<=n; i++){
+            for(int j=1; j<=amt; j++){
+                int take = (int)1e9;
+                if(coins[i-1] <= j){
+                    take = 1+dp[i][j-coins[i-1]];
+                }
+                int ntake = dp[i-1][j];
+                dp[i][j] = Math.min(ntake,take);
+            }
+        }
+        return dp[n][amt]==(int)1e9 ? -1 : dp[n][amt];
+    }
+}
