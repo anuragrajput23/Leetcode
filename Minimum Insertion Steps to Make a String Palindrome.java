19e701f3234db4ed2292e5dbf6cab9e121f9d400
anuragrajput23 <anuragtwo3@gmail.com>
2026-06-26 17:50:19 +0530

    tabulation

---
+++ Minimum Insertion Steps to Make a String Palindrome.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int minInsertions(String s) {
+        int n = s.length();
+        String s1 = "";
+        for(int i= n-1; i>=0; i--) s1 += s.charAt(i);
+        int[][] dp = new int[n+1][n+1];
+        for(int i=0; i<n+1; i++) dp[i][0] = 0;
+        for(int j=0; j<n+1; j++) dp[0][j] = 0;
+
+        for(int i=1; i<n+1; i++){
+            for(int j=1; j<n+1; j++){
+                if(s1.charAt(i-1)==s.charAt(j-1)){
+                    dp[i][j] = 1 + dp[i-1][j-1];
+                }else{
+                    dp[i][j]= Math.max(dp[i-1][j],dp[i][j-1]);
+                }
+            }
+        }
+        return n-dp[n][n];
+    }
+}
