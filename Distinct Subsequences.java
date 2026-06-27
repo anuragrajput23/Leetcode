0f13a454d33bb3b4ee2be3c2e54327cd922231dc
anuragrajput23 <anuragtwo3@gmail.com>
2026-06-27 17:18:29 +0530

    memoisation

--- Distinct Subsequences.java
+++ Distinct Subsequences.java
@@ -14,3 +14,24 @@ class Solution {
         }
     }
 }
+
+//memoisation
+class Solution {
+    Integer[][] dp;
+    public int numDistinct(String s, String t) {
+        int n = s.length();
+        int m = t.length();
+        dp = new Integer[n+1][m+1];
+        return solve(n-1,m-1,s,t);
+    }
+    public int solve(int i, int j, String s, String t){
+        if(j<0) return 1;
+        if(i<0) return 0;
+        if(dp[i][j]!=null) return dp[i][j];
+        if(s.charAt(i)==t.charAt(j)){
+            return dp[i][j]= solve(i-1,j-1,s,t) + solve(i-1,j,s,t);
+        }else{
+           return dp[i][j]= solve(i-1,j,s,t);
+        }
+    }
+}
