db5ffcd096ed71311e2d60720233d421af98f286
anuragrajput23 <anuragtwo3@gmail.com>
2026-07-06 23:44:27 +0530

    optimal

--- Maximum Valid Pair Sum.java
+++ Maximum Valid Pair Sum.java
@@ -10,3 +10,19 @@ class Solution {
         return ans;
     }
 }
+
+class Solution {
+    public int maxValidPairSum(int[] nums, int k) {
+        int n = nums.length;
+        int ans = 0;
+        int[] suf = new int[n];
+        suf[n-1]=nums[n-1];
+        for(int i=n-2; i>=0; i--){
+            suf[i]= Math.max(suf[i+1],nums[i]);
+        }
+        for(int i=0; i<n-k; i++){
+            ans = Math.max(ans, nums[i]+suf[i+k]);
+        }
+        return ans;
+    }
+}
