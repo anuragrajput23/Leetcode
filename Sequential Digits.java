91812bafba92b54e221fd81cc71af0a0e579d901
anuragrajput23 <anuragtwo3@gmail.com>
2026-07-13 17:40:56 +0530

    optimal

--- Sequential Digits.java
+++ Sequential Digits.java
@@ -18,3 +18,33 @@ class Solution {
         return true;
     }
 }
+
+//More optimised O(N) and O(N);
+class Solution {
+    public List<Integer> sequentialDigits(int low, int high) {
+        List<Integer> list = new ArrayList<>();
+        Queue<Integer> qu = new LinkedList<>();
+        qu.offer(1);
+        qu.offer(2);
+        qu.offer(3);
+        qu.offer(4);
+        qu.offer(5);
+        qu.offer(6);
+        qu.offer(7);
+        qu.offer(8);
+        //bfs
+        while(!qu.isEmpty()){
+            int front = qu.poll();
+            if(front >= low && front <= high){
+                list.add(front);
+            }
+            int lastElem = front%10;
+            if(lastElem < 9){
+                int nextElem = front*10+lastElem+1;
+                if(nextElem <= high) qu.offer(nextElem);
+
+            }
+        }
+        return list;
+    }
+}
