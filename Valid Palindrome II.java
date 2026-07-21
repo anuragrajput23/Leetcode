a6b70f4e940004470616c81080642b01c2020799
anuragrajput23 <anuragtwo3@gmail.com>
2026-07-21 10:53:06 +0530

    clean code

--- Valid Palindrome II.java
+++ Valid Palindrome II.java
@@ -27,3 +27,31 @@ class Solution {
         return false;
     }
 }
+
+//clean code
+class Solution {
+    public boolean isPalindrome(String s, int i, int j){
+
+        while(i<j){
+            if(s.charAt(i)==s.charAt(j)){
+                i++;
+                j--;
+            }else{
+                return false;
+            }
+        }
+        return true;
+    }
+    public boolean validPalindrome(String s) {
+        int n = s.length();
+        int i=0,j=n-1;
+        while(i<j){
+            if(s.charAt(i)==s.charAt(j)){
+                i++; j--;
+            }else{
+                return isPalindrome(s,i+1,j) || isPalindrome(s,i,j-1);
+            }
+        }
+        return true;
+    }
+}
