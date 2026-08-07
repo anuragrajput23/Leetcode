071ceb82507fa757b220dab4764f2f9000f283f3
anuragrajput23 <anuragtwo3@gmail.com>
2026-08-07 17:34:29 +0530

    math

---
+++ Smallest Divisible Digit Product I.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int smallestNumber(int n, int t) {
+        while(!check(n,t)){
+            n++;
+        }
+        return n;
+    }
+    public boolean check(int n, int t){
+        int prod = 1;
+        while(n!=0){
+            prod *= n%10;
+            n = n/10;
+        }
+        if(prod%t==0) return true;
+        return false;
+    }
+}
