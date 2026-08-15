527d1b13307fbdd21e51de95327c6309521625a7
anuragrajput23 <anuragtwo3@gmail.com>
2026-08-15 11:23:22 +0530

    space optimisation

--- Permutations.java
+++ Permutations.java
@@ -23,3 +23,33 @@ class Solution {
         }
     }
 }
+
+//space O(1)
+class Solution {
+    public List<List<Integer>> permute(int[] nums) {
+        List<List<Integer>> ans = new ArrayList<>();
+        List<Integer> list = new ArrayList<>();
+        solve(0,nums,ans);
+        return ans;
+    }
+    public void solve(int idx, int[] nums, List<List<Integer>> ans){
+        if(idx==nums.length){
+            List<Integer> list = new ArrayList<>();
+            for(int ele : nums){
+                list.add(ele);
+            }
+            ans.add(list);
+            return;
+        }
+        for(int i=idx; i<nums.length; i++){
+            swap(i,idx,nums);
+            solve(idx+1,nums,ans);
+            swap(i,idx,nums);
+        }
+    }
+    public void swap(int i,int j, int[] nums){
+        int temp= nums[i];
+        nums[i] = nums[j];
+        nums[j]=temp;
+    }
+}
