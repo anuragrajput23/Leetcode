9d6800eb9cb507d43f9af54e511bed7b9643251c
anuragrajput23 <anuragtwo3@gmail.com>
2026-08-16 16:55:30 +0530

    slightly optimised

--- Combination Sum III.java
+++ Combination Sum III.java
@@ -23,3 +23,32 @@ class Solution {
         }
     }
 }
+
+//slight optimised
+class Solution {
+    public List<List<Integer>> combinationSum3(int k, int n) {
+        List<List<Integer>> ans = new ArrayList<>();
+        List<Integer> list = new ArrayList<>();
+        solve(1,k,n,ans,list);
+        return ans;
+    }
+    public void solve(int idx,int k, int target, List<List<Integer>> ans, List<Integer> list){
+        if(target==0){
+            if(list.size()==k){
+                ans.add(new ArrayList<>(list));
+            }
+            return;
+        }
+        if(list.size()==k) return;
+        for(int i=idx; i<=9; i++){
+            if(i<=target){
+                 if (i > target) {
+                    break;
+                }
+                list.add(i);
+                solve(i+1,k,target-i,ans,list);
+                list.remove(list.size()-1);
+            }
+        }
+    }
+}
