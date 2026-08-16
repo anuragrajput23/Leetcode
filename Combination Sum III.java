08e7b44e4d07de0f6aecec302396005631295964
anuragrajput23 <anuragtwo3@gmail.com>
2026-08-16 16:40:25 +0530

    backtracking

---
+++ Combination Sum III.java
@@ -0,0 +1,25 @@
+class Solution {
+    public List<List<Integer>> combinationSum3(int k, int n) {
+        List<List<Integer>> ans = new ArrayList<>();
+        List<Integer> list = new ArrayList<>();
+        solve(1,k,n,ans,list);
+        return ans;
+    }
+    public void solve(int idx,int k, int target, List<List<Integer>> ans, List<Integer> list){
+        if(target==0){
+            if(list.size()==k){
+                ans.add(new ArrayList<>(list));
+                return;
+            }else{
+                return;
+            }
+        }
+        for(int i=idx; i<=9; i++){
+            if(i<=target){
+                list.add(i);
+                solve(i+1,k,target-i,ans,list);
+                list.remove(list.size()-1);
+            }
+        }
+    }
+}
