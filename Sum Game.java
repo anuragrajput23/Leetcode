5502a74f4abebcbd702b4e6875c5383da5915b76
anuragrajput23 <anuragtwo3@gmail.com>
2026-08-23 23:43:50 +0530

    math

---
+++ Sum Game.java
@@ -0,0 +1,28 @@
+class Solution {
+    public boolean sumGame(String num) {
+        char[] arr = num.toCharArray();
+        int n = num.length();
+        int i=0;
+        int j=n-1;
+        int leftSum = 0;
+        int rightSum = 0;
+        int t1 = 0;
+        int t2 = 0;
+        while(i < j){
+            if(arr[i]!='?'){
+                leftSum += arr[i]-'0';
+            }else{
+                t1++;
+            }
+            if(arr[j]!='?'){
+                rightSum += arr[j]-'0';
+            }else{
+                t2++;
+            }
+            i++;
+            j--;
+        }
+        return 2 * (leftSum - rightSum) != 9 * (t2 - t1);
+
+    }
+}
