9ddc2d2626c0d7abc1d658a78cfbc90616d4deb9
anuragrajput23 <anuragtwo3@gmail.com>
2026-08-24 14:43:09 +0530

    slight optimisation

--- Sum Game.java
+++ Sum Game.java
@@ -26,3 +26,30 @@ class Solution {
 
     }
 }
+
+//slight optimisation
+class Solution {
+    public boolean sumGame(String num) {
+        char[] arr = num.toCharArray();
+        int n = num.length();
+        int i=0;
+        int leftSum = 0;
+        int rightSum = 0;
+        int t1 = 0;
+        int t2 = 0;
+        while(i < n/2){
+            if(arr[i]!='?'){
+                leftSum += arr[i]-'0';
+            }else{
+                t1++;
+            }
+            if(arr[n-1-i]!='?'){
+                rightSum += arr[n-1-i]-'0';
+            }else{
+                t2++;
+            }
+            i++;
+        }
+        return 2 * (leftSum - rightSum) != 9 * (t2 - t1);
+    }
+}
