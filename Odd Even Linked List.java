0254f8255d2cebae84ffbf76fd94fe0e7ada74e7
anuragrajput23 <anuragtwo3@gmail.com>
2026-09-02 16:14:09 +0530

    optimised space

--- Odd Even Linked List.java
+++ Odd Even Linked List.java
@@ -37,3 +37,22 @@ class Solution {
         return ans.next;
     }
 }
+
+//sc= O(1)
+class Solution {
+    public ListNode oddEvenList(ListNode head) {
+        if(head==null || head.next==null) return head;
+        ListNode even = head.next;
+        ListNode odd = head;
+        ListNode newHead = head.next;
+        while(even!=null && even.next!=null){
+            odd.next = odd.next.next;
+            even.next = even.next.next;
+
+            odd = odd.next;
+            even = even.next;
+        }
+        odd.next = newHead;
+        return head;
+    }
+}
