97b7802368119f6d641ab4df6b4cb7df375a9b6f
anuragrajput23 <anuragtwo3@gmail.com>
2026-09-07 21:46:53 +0530

    recursive

--- Reverse Linked List.java
+++ Reverse Linked List.java
@@ -21,3 +21,18 @@ class Solution {
         return prev;
     }
 }
+
+//recursive way
+
+class Solution {
+    public ListNode reverseList(ListNode head) {
+        if(head==null || head.next==null) return head;
+
+        ListNode newHead = reverseList(head.next);
+        ListNode front = head.next;
+        front.next = head;
+        head.next = null;
+        return newHead;
+
+    }
+}
